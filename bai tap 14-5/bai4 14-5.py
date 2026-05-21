9a9848f7ae21a9c81ac469a0d1d1fa3bac8e499f
NguyenHoa <153062522+viethoa2004z@users.noreply.github.com>
2026-05-21 22:19:43 +0700

    Delete bai tap 14-5 directory

--- bai tap 14-5/bai4 14-5.py
+++
@@ -1,9 +0,0 @@
-m = int(input("Nhập số m: "))
-n = int(input("Nhập số n: "))
-
-tong = m + n
-print("Tổng =", tong)
-
-chuoi_tong = str(tong)
-max_digit = int(max(chuoi_tong))
-print("Chữ số lớn nhất trong tổng là:", max_digit)
